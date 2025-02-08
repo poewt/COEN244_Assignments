99dd6eb1fc33dc2588a5bfd737e1d0bea956be6c
poewt <79345528+poewt@users.noreply.github.com>
2025-02-08 11:36:57 -0500

    Init custom invoice testing + negative

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -33,7 +33,31 @@ void testInvoice()
     // Testing getInvoiceAmount
     cout << "New invoice amount: " << invoice.getInvoiceAmount() << "\n" << endl;
 
+    // Testing negative amounts
+    invoice.setQtyPurchased(-5);
+    cout << "Negative qty invoice amount: " << invoice.getInvoiceAmount() << "\n" << endl;
+
+    invoice.setQtyPurchased(5);
+    invoice.setPricePerUnit(-12);
+    cout << "Negative price per unit invoice amount: " << invoice.getInvoiceAmount() << "\n" << endl;
+
+
     // Testing second constructor
+    Invoice testInvoice(
+        "B0009",
+        "This model focuses on high quality parts.",
+        240,
+        11
+        );
+
+    cout << "Custom invoice parameters: " << endl;
+    cout << "Part Number: " << testInvoice.getPartNumber() << endl;
+    cout << "Part Desc: " << testInvoice.getPartDesc() << endl;
+    cout << "Quantity Purchased: " << testInvoice.getQtyPurchased() << endl;
+    cout << "Price Per Unit: " << testInvoice.getPricePerUnit() << "\n" << endl;
+
+    cout << "Custom invoice amount: " << testInvoice.getInvoiceAmount() << "\n" << endl;
+
 }
 
 int main()
