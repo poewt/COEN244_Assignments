1a0dc6485a5deaabad3c09cae1e8bd5477004bed
poewt <79345528+poewt@users.noreply.github.com>
2025-02-08 11:27:18 -0500

    Init invoice testing - getters

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include "Invoice.h"
+
+using namespace std;
+
+void testInvoice()
+{
+    Invoice invoice;
+
+    // Testing getters
+    cout << "Default invoice parameters: " << endl;
+    cout << "Part Number: " << invoice.getPartNumber() << endl;
+    cout << "Part Desc: " << invoice.getPartDesc() << endl;
+    cout << "Quantity Purchased: " << invoice.getQtyPurchased() << endl;
+    cout << "Price Per Unit: " << invoice.getPricePerUnit() << endl;
+
+    // Testing setters
+
+
+    // Testing second constructor
+
+    // Testing getInvoiceAmount
+
+
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "============ Start - Function Testing ============" << endl;
+    testInvoice();
+    cout << "============ End - Function Testing ==============" << endl;
+
     return 0;
 }
